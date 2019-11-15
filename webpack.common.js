0329035ed83a3027b3a17d2df258f94df5352eb4
method7 <simon@method7.co.uk>
2019-11-15 10:13:42 +0000

    tinkerings

--- webpack.common.js
+++ webpack.common.js
@@ -1,38 +1,36 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+import HtmlWebPackPlugin from "html-webpack-plugin";
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      },
-      {
-        enforce: "pre",
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader"
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+export const module = {
+  rules: [
+    {
+      test: /\.html$/,
+      use: [
+        {
+          loader: "html-loader"
         }
+      ]
+    },
+    {
+      enforce: "pre",
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: "eslint-loader"
+    },
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
       }
-    ]
-  },
-  resolve: {
-    extensions: [".jsx", ".js", ".json"]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
-    })
+    }
   ]
 };
+export const resolve = {
+  extensions: [".jsx", ".js", ".json"]
+};
+export const plugins = [
+  new HtmlWebPackPlugin({
+    template: "./public/index.html",
+    filename: "./index.html"
+  })
+];
